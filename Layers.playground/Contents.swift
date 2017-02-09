1d88a8f01283354f796696fe3c2c88cee481d764
Madhur Malhotra <malhotra.madhur15@gmail.com>
2017-02-09 11:13:43 -0800

    completed challenge 2

--- Layers.playground/Contents.swift
+++ Layers.playground/Contents.swift
@@ -47,4 +47,33 @@ let boardView2 = UIView(frame: boardFrame)
 boardView2.layer.borderWidth = 15
 boardView2.layer.borderColor = UIColor.black.cgColor
 boardView2.clipsToBounds = true
+
+let lightImage = UIImage(named: "light.png")
+let darkImage = UIImage(named: "dark.png")
+
+for j in 0..<8 {
+    let rowFrame2 = CGRect(x: 0, y:CGFloat((superFrame.width/8)*CGFloat(j)), width: superFrame.width, height: superFrame.width/8)
+    let rowView2 = UIView(frame: rowFrame2)
+    for i in 0..<8 {
+        let checkFrame2 = CGRect(x:CGFloat((superFrame.width/8)*CGFloat(i)), y:0, width: superFrame.width/8, height: superFrame.width/8)
+        let checkView2 = UIView(frame: checkFrame2)
+        if (j%2) == 0{
+            if (i%2) == 0 {
+                checkView2.backgroundColor = UIColor(patternImage: darkImage!)
+            } else {
+                checkView2.backgroundColor = UIColor(patternImage: lightImage!)
+            }
+        } else {
+            if (i%2) == 0 {
+                checkView2.backgroundColor = UIColor(patternImage: lightImage!)
+            } else {
+                checkView2.backgroundColor = UIColor(patternImage: darkImage!)
+            }
+        }
+        rowView2.addSubview(checkView2)
+    }
+    boardView2.addSubview(rowView2)
+}
 superView3.addSubview(boardView2)
+
+
